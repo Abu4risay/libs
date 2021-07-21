50fcbb85f2b2263947f853cda4fc5b481ec22476
Abu4risay <abu4risay1@gmail.com>
2021-07-21 10:29:56 +0100

    hi

---
+++ src/main/java/com/mycompany/library/LibraryActivity.java
@@ -0,0 +1,25 @@
+package com.mycompany.library;
+
+import android.app.*;
+import android.os.*;
+import android.widget.*;
+
+public class LibraryActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.library);
+    
+		
+		
+		
+		}
+		
+					
+	
+		
+		
+		
+}
---
+++ src/main/java/com/mycompany/library/Maths.java
@@ -0,0 +1,13 @@
+package com.mycompany.library;
+
+public class Maths
+{
+	
+	public static int Plus(int a, int b){
+		return a+b;
+	}
+	public static int Minus(int a, int b){
+		return a-b;
+	}
+	
+}
