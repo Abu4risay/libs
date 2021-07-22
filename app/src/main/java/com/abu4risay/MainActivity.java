d523dc5dc3d3a62f2b56727610489cb06cd71508
Abu4risay <abu4risay1@gmail.com>
2021-07-22 11:09:43 +0100

    a

---
+++ app/src/main/java/com/abu4risay/MainActivity.java
@@ -0,0 +1,14 @@
+package com.abu4risay;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
